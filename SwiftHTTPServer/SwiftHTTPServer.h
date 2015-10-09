4e3ac171bf190957e601531cf769478ea0da1a49
Tyler Long <tyler4long@gmail.com>
2015-10-09 22:05:56 +0800

    Initial Commit

---
+++ SwiftHTTPServer/SwiftHTTPServer.h
@@ -0,0 +1,19 @@
+//
+//  SwiftHTTPServer.h
+//  SwiftHTTPServer
+//
+//  Created by Tyler Long on 9/10/2015.
+//  Copyright © 2015 Tylingsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftHTTPServer.
+FOUNDATION_EXPORT double SwiftHTTPServerVersionNumber;
+
+//! Project version string for SwiftHTTPServer.
+FOUNDATION_EXPORT const unsigned char SwiftHTTPServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftHTTPServer/PublicHeader.h>
+
+
